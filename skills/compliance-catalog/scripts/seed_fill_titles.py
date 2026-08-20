879ea1f8cdd40090a61fce53761ffbd3056d768d
hirokuni-kitahara <hirokuni.kitahara1@ibm.com>
2026-08-20 13:50:05 +0900

    Add license headers to compliance-catalog and compliance-mapping scripts.
    Signed-off-by: Hirokuni Kitahara <hirokuni.kitahara1@ibm.com>

--- skills/compliance-catalog/scripts/seed_fill_titles.py
+++ skills/compliance-catalog/scripts/seed_fill_titles.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+# Copyright OSCAL Compass Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Fill empty control / group titles in a newly generated OSCAL Catalog
 from the corresponding titles in a *seed catalog* (a previous generation
 of the same framework).
